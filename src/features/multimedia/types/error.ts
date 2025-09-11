4fb9a0ddeca1857c0b38eac1b4fe3da290ef2816
Alexandre Fernandes Costa <fcosta.xand@gmail.com>
2025-09-11 12:17:00 -0300

    fix: multimedia error type

--- src/features/multimedia/types/error.ts
+++ src/features/multimedia/types/error.ts
@@ -1,7 +1,12 @@
 import type { AudioError } from "@/features/multimedia/audio/types/error";
 import type { MicError } from "@/features/multimedia/microphone/types/error";
 
-export type MultimediaError = {
-    type: "audio" | "microphone";
-    reason: MicError | AudioError;
-};
+export type MultimediaError =
+    | {
+          type: "audio";
+          reason: AudioError;
+      }
+    | {
+          type: "microphone";
+          reason: MicError;
+      };
